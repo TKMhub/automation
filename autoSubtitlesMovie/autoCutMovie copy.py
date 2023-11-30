7f4bcfeb40938e62469abfe8cd31084bb0d72245
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-11-30 23:48:06 +0900

    1130

--- autoSubtitlesMovie/autoCutMovie copy.py
+++ autoSubtitlesMovie/autoCutMovie copy.py
@@ -1,53 +1,52 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-from pydub import AudioSegment
-from pydub.silence import detect_nonsilent
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+import speech_recognition as sr
 import os
 
-def detect_silence(audio_segment, min_silence_len=500, silence_thresh=-50):
-    # 非無音部分を検出
-    nonsilent_parts = detect_nonsilent(
-        audio_segment,
-        min_silence_len=min_silence_len,
-        silence_thresh=silence_thresh
-    )
-    return nonsilent_parts
-
-def cut_video(clip, nonsilent_parts):
-    # カットする部分を集める
-    clips = []
-    for start, end in nonsilent_parts:
-        start_time = start / 1000.0  # PyDubはミリ秒単位で時間を扱うため、秒単位に変換
-        end_time = end / 1000.0
-        if start_time < clip.duration and end_time <= clip.duration:
-            subclip = clip.subclip(start_time, end_time)
-            clips.append(subclip)
-
-    # クリップを結合
-    if clips:
-        final_clip = concatenate_videoclips(clips)
-        return final_clip
-    else:
+# 音声をテキストに変換する関数
+def speech_to_text(audio_file):
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+    try:
+        text = r.recognize_google(audio)
+        return text
+    except Exception as e:
+        # 音声認識が失敗した場合のエラーハンドリング
+        print(f"音声認識に失敗しました: {e}")
         return None
 
-def process_video(video_path):
-    # ビデオを読み込む
+# 字幕を生成して動画に追加する関数
+def add_subtitles(video_path, text, subtitle_style):
+    # 動画を読み込む
     clip = VideoFileClip(video_path)
-    # 音声がない部分を検出
-    audio_segment = AudioSegment.from_file(video_path, format="mp4")
-    silence_ranges = detect_silence(audio_segment)
-    # カットする部分を決定
-    cut_ranges = silence_ranges
-    # カット
-    final_clip = cut_video(clip, silence_ranges)
+    # 字幕クリップを生成する
+    subtitle_clip = TextClip(text, **subtitle_style).set_position('bottom').set_duration(clip.duration)
+    # 字幕を動画に合成する
+    video = CompositeVideoClip([clip, subtitle_clip])
     # 結果を保存
-    if final_clip is not None:
-        output_path = os.path.splitext(video_path)[0] + '_cut.mp4'
-        # 音声のビットレートを調整し、適切なコーデックを指定
-        final_clip.write_videofile(output_path, audio_codec='aac', audio_bitrate='192k', preset='slow', ffmpeg_params=['-crf', '18'])
+    output_path = os.path.splitext(video_path)[0] + '_subtitled.mp4'
+    video.write_videofile(output_path)
+
+def process_video_with_subtitles(video_path, subtitle_style):
+    # ビデオの音声を抽出
+    audio_path = os.path.splitext(video_path)[0] + '.wav'
+    video = VideoFileClip(video_path)
+    video.audio.write_audiofile(audio_path)
+    # 音声をテキストに変換
+    text = speech_to_text(audio_path)
+    if text:
+        # 字幕を動画に追加
+        add_subtitles(video_path, text, subtitle_style)
+
+# 字幕のスタイル設定（例）
+subtitle_style = {
+    'fontsize': 24,
+    'color': 'white'
+}
 
 # ディレクトリ内の全動画ファイルに対して処理を実行
-directory = "/Users/tkm/Documents/autoCutMovie"
+directory = "/path/to/your/videos"
 for filename in os.listdir(directory):
     file_path = os.path.join(directory, filename)
     if filename.endswith(".mp4") and os.path.isfile(file_path):
-        process_video(file_path)
+        process_video_with_subtitles(file_path, subtitle_style)
